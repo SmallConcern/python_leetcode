687a67929df90d4506fd35689b7cefca7cfc7cf0
casey small <casey small>
2017-03-06 21:23:59 -0600

    Fixed longest palindrome to work.

--- 5_longest_palindrome/longest_palindrome.py
+++ 5_longest_palindrome/longest_palindrome.py
@@ -1,21 +1,19 @@
-
 def find_longest_palindrome(input_str):
+    if len(input_str) == 0:
+        return ''
+    elif len(input_str) == 1:
+        return input_str
     longest_palindrome = ''
-    for start in xrange(1, len(input_str)-2):
-        distance_from_end = min(len(input_str)-1-start, start)
-        right_modifier = 0
-        if input_str[start] == input_str[start+1]:
-            right_modifier = 1
-            longest_palindrome = max(longest_palindrome, input_str[start:start+2])
-        for j in xrange(1, distance_from_end+1):
-            if input_str[start-j] != input_str[start+j+right_modifier]:
-                break
-            else:
-                palindrome = input_str[start - j:start + j + 1 + right_modifier]
-                if len(palindrome) > len(longest_palindrome):
-                    longest_palindrome = palindrome
+    for start in xrange(0, len(input_str)):
+        left = right = start
+        while right < len(input_str)-1 and input_str[right+1] == input_str[right]:
+            right += 1
+        while right < len(input_str)-1 and left > 0 and input_str[left-1] == input_str[right+1]:
+            right += 1
+            left -= 1
+        if right - left + 1 > len(longest_palindrome):
+            longest_palindrome = input_str[left:right+1]
     return longest_palindrome
-
 
 class Solution(object):
     def longestPalindrome(self, s):
@@ -23,10 +21,11 @@ class Solution(object):
 
 class TestLongestPalindrome(object):
     def test_find_longest_palindrome(self):
-        # assert find_longest_palindrome("babad") == "bab"
-        # assert find_longest_palindrome("cbbd") == "bb"
-        # assert find_longest_palindrome("dbbd") == "dbbd"
-        # assert find_longest_palindrome("abcdefghihgfedcba") == "abcdefghihgfedcba"
-        # assert find_longest_palindrome("abcdefghiihgfedcba") == "abcdefghiihgfedcba"
-        # assert find_longest_palindrome("banana") == "anana"
+        assert find_longest_palindrome("babad") == "bab"
+        assert find_longest_palindrome("cbbd") == "bb"
+        assert find_longest_palindrome("dbbd") == "dbbd"
+        assert find_longest_palindrome("abcdefghihgfedcba") == "abcdefghihgfedcba"
+        assert find_longest_palindrome("abcdefghiihgfedcba") == "abcdefghiihgfedcba"
+        assert find_longest_palindrome("banana") == "anana"
         assert find_longest_palindrome("aaabaaaa") == "aaabaaa"
+        assert find_longest_palindrome("bb") == "bb"
