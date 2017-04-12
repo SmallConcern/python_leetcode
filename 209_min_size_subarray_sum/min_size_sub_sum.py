b638bc7ef184dfb1e4ff627443b0ce664ece7ea7
Casey Small <kcsmall00@gmail.com>
2017-04-11 22:07:50 -0500

    Added / modified a few problems.

--- 209_min_size_subarray_sum/min_size_sub_sum.py
+++ 209_min_size_subarray_sum/min_size_sub_sum.py
@@ -1,13 +1,19 @@
+import sys
+
 class Solution(object):
     def minSubArrayLen(self, s, nums):
-        """
-        :type s: int
-        :type nums: List[int]
-        :rtype: int
-        """
+        total, left = 0, 0
+        min_size = len(nums) + 1
+        for right, n in enumerate(nums):
+            total += n
+            while total >= s:
+                min_size = min(min_size, right - left + 1)
+                total -= nums[left]
+                left += 1
+        return min_size if min_size <= len(nums) else 0
 
 
 class TestMaximumSubarray(object):
     def test_maximum_subarray(self):
         s = Solution()
-        assert s.minSubArrayLen([2,3,1,2,4,3], 7) == 2
+        assert s.minSubArrayLen(7, [2,3,1,2,4,3]) == 2
